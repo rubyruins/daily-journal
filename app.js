3ce58ecaa27c335b83c49231389adaca7ad1965b
rubyruins <soumya.parekh@somaiya.edu>
2020-04-05 23:29:04 +0530

    added about, contact and compose pages

--- app.js
+++ app.js
@@ -17,6 +17,23 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res) {
   res.render("home", {homeStartingContent: homeStartingContent});
+});
+
+app.get("/about", function(req, res) {
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res) {
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res) {
+  let sentence = req.body.newPost;
+  console.log(sentence);
 })
 
 app.listen(3000, function() {
